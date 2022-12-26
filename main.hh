2a22425ad274d5c7ba6cdf5f5f7a96836a794f41
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2022-12-26 15:17:50 +0200

    Only get first level tags in body

--- main.hh
+++ main.hh
@@ -3,10 +3,48 @@
 #include <FlexLexer.h>
 #endif
 
+#include <vector>
+
 class HTMLParser : public yyFlexLexer
 {
     public:
         HTMLParser(std::istream& arg_yyin, std::ostream& arg_yyout) : yyFlexLexer(arg_yyin, arg_yyout) {}
 	    virtual ~HTMLParser() {}
         virtual int yylex();
+};
+
+class Tag
+{
+    public:
+        Tag(std::string name)
+        {
+            this->name = name;
+            this->name[name.size() - 1] = '>';
+        }
+        std::string name;
+        std::vector<Tag> subtags;
+        std::string toString(bool withName = true)
+        {
+            std::string result = "";
+            if (withName)
+            {
+                result += name;
+                if (subtags.size() == 0)
+                {
+                    return result;
+                }
+                result += "->";
+            }
+            result += "[";
+            for (int i = 0; i < subtags.size(); i++)
+            {
+                result += subtags[i].toString();
+                if (i != subtags.size() - 1)
+                {
+                    result += ",";
+                }
+            }
+            result += "]";
+            return result;
+        }
 };
